3015f28c3a637ec4a464712d807f35e242350524
Rafael Mejia <rafaeladad23@gmail.com>
2022-03-20 19:36:55 -0600

    responsive nav-bar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,24 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Nav extends Component {
   render() {
     return (
       <>
-      <div className='nav__div'>
-        <a className='logo' href='#home'>RAMB.code</a>
-        <nav>
-            <ul>
-                <a className='nav__item' href='#edu'>Education</a>
-                <a className='nav__item' href='#exp'>Experience</a>
-                <a className='nav__item' href='#projects'>Projects</a>
-                <a className='nav__item' href='#about'>About me</a>
-                <a className='nav__item' href='#contact'>Contact me</a>
+        <header className="header">
+          <a className="logo" href="#home">
+            RAMB.code
+          </a>
+          <nav className="nav">
+            <ul className="nav-menu">
+              <li className="nav-menu-item">
+                <a className="nav__item" href="#edu">
+                  Education
+                </a>
+              </li>
+              <li className="nav-menu-item">
+                <a className="nav__item" href="#exp">
+                  Experience
+                </a>
+              </li>
+              <li className="nav-menu-item">
+                <a className="nav__item" href="#projects">
+                  Projects
+                </a>
+              </li>
+              <li className="nav-menu-item">
+                <a className="nav__item" href="#about">
+                  About me
+                </a>
+              </li>
+              <li className="nav-menu-item">
+                <a className="nav__item" href="#contact">
+                  Contact me
+                </a>
+              </li>
             </ul>
-        </nav>
-        <a className='logo__ghost' href='#home'>RAMB.code</a>
-      </div>
+          </nav>
+          <a className="logo__ghost" href="#home">
+            RAMB.code
+          </a>
+        </header>
       </>
-    )
+    );
   }
 }
-
